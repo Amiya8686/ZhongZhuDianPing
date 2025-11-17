41485a9aaf1944d09eccc2e83744c6583968dc22
Amiya8686 <1580714256@qq.com>
2025-11-17 16:05:28 +0800

    repair personalInfor bug

--- src/pages/personalInfo/personalInfo.vue
+++ src/pages/personalInfo/personalInfo.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {reactive, getCurrentInstance, onMounted, ref} from "vue"
 import {defaultUserInfo} from "@/config/defaultUserInfo"
+import { ElMessage } from "element-plus"
 const {proxy} = getCurrentInstance()
 
 //用户信息
@@ -75,25 +76,31 @@ const handleCancel = ()=>{
 }
 //函数: 提交表单
 const submitForm = async ()=>{
-  isLoading.value=true
-  //生成formData
-  const formData = new FormData()
-  if(avatarFile.value!=null){
-    formData.append('avatar',avatarFile.value)
-  }
-  formData.append('nickName',userInfo.nickName)
+    proxy.$refs["editBox"].validate(async (valid)=>{
+      if(valid){
+        isLoading.value=true
+        //生成formData
+        const formData = new FormData()
+        if(avatarFile.value!=null){
+          formData.append('avatar',avatarFile.value)
+        }
+        formData.append('nickName',userInfo.nickName)
 
-  //发送请求
-  try{
-    await proxy.$userApi.editUserInfo(formData)
-    ElMessage.success("修改成功")
-    loadUserInfo()
-  }catch(error){
-    console.log(error)
-    ElMessage.error(error || "修改失败")
-  }finally{
-    isLoading.value = false
-  }
+        //发送请求
+        try{
+          await proxy.$userApi.editUserInfo(formData)
+          ElMessage.success("修改成功")
+          loadUserInfo()
+        }catch(error){
+          console.log(error)
+          ElMessage.error(error || "修改失败")
+        }finally{
+          isLoading.value = false
+        }
+      }else{
+        ElMessage.error("请输入正确的信息")
+      }
+  })
 }
 
 
