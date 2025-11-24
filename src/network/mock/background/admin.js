0ba1ae70183ddeceff6a5453607fed0c1b6ac340
Amiya8686 <1580714256@qq.com>
2025-11-24 15:11:41 +0800

    aa

--- src/network/mock/background/admin.js
+++ src/network/mock/background/admin.js
@@ -39,7 +39,7 @@ const checkToken = (config)=>{
     if(token){
         const ID = token
         const index = adminDataBase.find(item=>{return item.ID===ID})
-        if(index!=-1){
+        if(index){
             return ID
         }
     }
