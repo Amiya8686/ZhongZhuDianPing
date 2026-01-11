24a211137c0586c0b931524bb7cfc80ade6980b2
Amiya8686 <1580714256@qq.com>
2026-01-11 19:30:22 +0800

    aa

--- src/pages/editPassword/editPassword.vue
+++ src/pages/editPassword/editPassword.vue
@@ -97,11 +97,7 @@ const submitForm = async ()=>{
 const isShowPage = ref(false);
 const tokenVerify = async ()=>{
   try{
-<<<<<<< HEAD
     await proxy.$tokenApi.checkToken();
-=======
-    await proxy.$tokenApi.chechToken();
->>>>>>> origin/develop1.1_user
     isShowPage.value = true;
   }catch(error){
     console.log(error);
